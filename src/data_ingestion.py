ba62097e59a56eb8fe00be18950735cbff1e8920
Harsh <yada30765@gmail.com>
2026-02-03 21:47:01 +0530

    param configured for pipeline

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -11,6 +11,7 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
 from sklearn import metrics
+import yaml
 
 # 1. LOGGING CONFIGURATION
 log_dir = 'logs'
@@ -30,6 +31,23 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a YAML file."""
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        logger.debug('Parameters retrieved from %s', params_path)
+        return params
+    except FileNotFoundError:
+        logger.error('File not found: %s', params_path)
+        raise
+    except yaml.YAMLError as e:
+        logger.error('YAML error: %s', e)
+        raise
+    except Exception as e:
+        logger.error('Unexpected error: %s', e)
+        raise
 
 # 2. FUNCTION DEFINITIONS
 
@@ -115,7 +133,9 @@ def main():
         final_df = preprocess_data(df)
         
         # Step 3: Splitting
-        train_df, test_df = train_test_split(final_df, test_size=0.2, random_state=42)
+        params = load_params(params_path='params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
         
         # Step 4: Save Raw Splits (Your requested format)
         save_data(train_df, test_df, data_path='./data')
